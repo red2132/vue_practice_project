eefd96bd8684a0f53b603430208c9acaeecbceae
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-10-10 16:41:21 +0900

    초기 환경설정

---
+++ vue3-posts/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HeaderComponent from './layouts/HeaderComponent.vue';
+import ViewComponent from './layouts/ViewComponent.vue';
+</script>
+
+<template>
+	<HeaderComponent />
+	<ViewComponent />
+</template>
---
+++ vue3-posts/src/layouts/ViewComponent.vue
@@ -0,0 +1,13 @@
+<template>
+	<main>
+		<div class="container py-4">
+			<h2>Helllo vue3</h2>
+		</div>
+	</main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>

4b36a6ed01879ab32a86f4d603cbe7d12866af69
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-10-11 19:05:27 +0900

    이벤트 처리

---
+++ vue3-basic-project/src/lecture/App_event.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button @click="printEventInfo('hello Vue3', $event)">
+      안녕 vue3!!
+    </button>
+    <hr>
+    <input type="text" @keyup="onKeyHandler">
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    const printEventInfo =(message, event) => {
+      console.log('message: '+message)
+      console.log('event.target: '+event.target)
+      console.log('event.target: '+event.target.tagName)
+    }
+
+    const onKeyHandler = event => {
+      console.log('event key: '+ event.key)
+    }
+
+    return {
+      printEventInfo,
+      onKeyHandler
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

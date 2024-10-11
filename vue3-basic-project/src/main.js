640d94d28f9b7c91f047123b36afcebf967f45e0
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-10-11 20:53:29 +0900

    watch+bootstrap 설치

--- vue3-basic-project/src/main.js
+++ vue3-basic-project/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import App from './App.vue';
 
 createApp(App).mount('#app');

888e74ce3f051126bb692acf7b433bbbe9bee5ee
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-10-11 15:07:34 +0900

    base project 환경 설정

---
+++ vue3-basic-project/vite.config.js
@@ -0,0 +1,15 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			// 경로 별칭
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+});

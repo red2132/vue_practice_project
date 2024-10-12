ac9729f4d5721dd5e54d41038570af724fbf4d8f
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-10-12 21:22:36 +0900

    slot 구현

--- vue3-basic-project/src/components/AppCard.vue
+++ vue3-basic-project/src/components/AppCard.vue
@@ -1,61 +1,36 @@
 <template>
-<div class="card">
-  <div class="card-body">
-		<span class="badge bg-secondary">{{ typeName }}</span>
-    <h5 class="card-title mt-2">{{ title }}</h5>
-    <p class="card-text">{{ contents }}</p>
-    <a href="#" :class="isLikeClass" @click="toggleLike">좋아요</a>
+  <div class="card">
+    <div class="card-header" v-if="$slots.header">
+      <slot name="header"></slot>
+    </div>
+    <div class="card-body">
+      <slot :child-message="childMessage"></slot>
+    </div>
+    <div class="card-footer text-muted" v-if="hasFooter">
+      <slot name="footer"></slot>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 export default {
-	props: {
-		type: {
-			type: String,
-			default: 'news',
-			validator: value => {
-				// 해당 값이 맞는지 확인
-				return ['news', 'notice'].includes(value);
-			}
-		},
-		title: {
-			type: String,
-			required: true,
-		},
-		contents: {
-			type: String,
-			// required: true,
-		},
-		isLike: {
-			type: Boolean,
-			default: false
-		}
-	},
-	emits: ['toggleLike'],
+  setup (props, {slots}) {
+    const childMessage = ref('자식 메시지');
 
-	setup(props, context) {
-		const isLikeClass = computed(() => 
-			props.isLike ? 'btn btn-danger' :'btn btn-outline-danger'
-		)
-		const typeName = computed(() => 
-			props.type === 'news'? '뉴스':'공지사항'
-		)
-		const toggleLike = () => {
-			//props.isLike = !props.isLike;
-			context.emit('toggleLike'); // 상위 컴포넌트로 이벤트 전송
-		}
-		
-		return { 
-			isLikeClass,
-			typeName,
-			toggleLike
-		};
-	},
-};
+    // setup의 context에서 slot 정보 가져올 수 있음
+    const hasFooter = computed(() => {
+      !!slots.footer
+    })
+    return {
+      childMessage,
+      hasFooter
+    }
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
---
+++ vue3-basic-project/src/components/PostIndex.vue
@@ -0,0 +1,61 @@
+<template>
+<div class="card">
+  <div class="card-body">
+		<span class="badge bg-secondary">{{ typeName }}</span>
+    <h5 class="card-title mt-2">{{ title }}</h5>
+    <p class="card-text">{{ contents }}</p>
+    <a href="#" :class="isLikeClass" @click="toggleLike">좋아요</a>
+  </div>
+</div>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+	props: {
+		type: {
+			type: String,
+			default: 'news',
+			validator: value => {
+				// 해당 값이 맞는지 확인
+				return ['news', 'notice'].includes(value);
+			}
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		contents: {
+			type: String,
+			// required: true,
+		},
+		isLike: {
+			type: Boolean,
+			default: false
+		}
+	},
+	emits: ['toggleLike'],
+
+	setup(props, context) {
+		const isLikeClass = computed(() => 
+			props.isLike ? 'btn btn-danger' :'btn btn-outline-danger'
+		)
+		const typeName = computed(() => 
+			props.type === 'news'? '뉴스':'공지사항'
+		)
+		const toggleLike = () => {
+			//props.isLike = !props.isLike;
+			context.emit('toggleLike'); // 상위 컴포넌트로 이벤트 전송
+		}
+		
+		return { 
+			isLikeClass,
+			typeName,
+			toggleLike
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>

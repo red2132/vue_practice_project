6db9659ee5e1ba3afe03c3422e3ff0ded636c2d5
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-10-15 14:36:13 +0900

    페이징 및 필터 구현

--- vue3-posts/src/views/posts/PostListView.vue
+++ vue3-posts/src/views/posts/PostListView.vue
@@ -2,6 +2,21 @@
 	<div>
 		<h2>Post List</h2>
 		<hr class="my-4" />
+		<form @submit.prevent>
+			<div class="row g-3">
+				<div class="col">
+					<input v-model="params.title_like" type="text" class="form-control" />
+				</div>
+				<div class="col-3">
+					<select v-model="params._limit" class="form-select">
+						<option value="3">3개씩 보기</option>
+						<option value="6">6개씩 보기</option>
+						<option value="9">9개씩 보기</option>
+					</select>
+				</div>
+			</div>
+		</form>
+		<hr class="my-4" />
 		<div class="row g-3">
 			<div v-for="post in posts" :key="post.id" class="col-4">
 				<PostItem
@@ -12,8 +27,45 @@
 				/>
 			</div>
 		</div>
-		<!-- <hr class="my-4" />
-		<AppCard>
+		<hr class="my-4" />
+		<nav aria-label="Page navigation example">
+			<ul class="pagination justify-content-center">
+				<li class="page-item" :class="{ disabled: !(params._page > 1) }">
+					<a
+						class="page-link"
+						href="#"
+						aria-label="Previous"
+						@click.prevent="--params._page"
+					>
+						<span aria-hidden="true">&laquo;</span>
+					</a>
+				</li>
+				<li
+					class="page-item"
+					v-for="page in pageCount"
+					:key="page"
+					:class="{ active: params._page === page }"
+				>
+					<a class="page-link" href="#" @click.prevent="params._page = page">{{
+						page
+					}}</a>
+				</li>
+				<li
+					class="page-item"
+					:class="{ disabled: !(params._page < pageCount) }"
+				>
+					<a
+						class="page-link"
+						href="#"
+						aria-label="Next"
+						@click.prevent="++params._page"
+					>
+						<span aria-hidden="true">&raquo;</span>
+					</a>
+				</li>
+			</ul>
+		</nav>
+		<!-- <AppCard>
 			<PostDetailView :id="1"></PostDetailView>
 		</AppCard> -->
 	</div>
@@ -21,21 +73,36 @@
 
 <script setup>
 import PostItem from '@/components/Posts/PostItem.vue';
-import { ref } from 'vue';
+import { ref, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
 import { getPosts } from '@/api/posts';
+import { computed } from 'vue';
 
 const posts = ref([]);
 const router = useRouter();
-
+const params = ref({
+	_sort: 'createdAt',
+	_order: 'desc',
+	_limit: 3,
+	_page: 1,
+	title_like: '제목',
+});
+//pagination
+const totalCount = ref(0);
+const pageCount = computed(() =>
+	Math.ceil(totalCount.value / params.value._limit),
+);
 const fetchPosts = async () => {
 	try {
-		const { data } = await getPosts();
+		const { data, headers } = await getPosts(params.value);
 		posts.value = data;
+		totalCount.value = headers['x-total-count'];
 	} catch (error) {
 		console.error(error);
 	}
 };
+
+watchEffect(fetchPosts);
 
 const goDetailPage = id => {
 	router.push({
